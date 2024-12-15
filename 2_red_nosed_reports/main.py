cb04bb0528dabd46d840c5a25f52f5e9123e8ac8
Tom Sitter <thomassiter@colabsoftware.com>
2024-12-15 17:18:58 -0330

    add solution for day 2

---
+++ 2_red_nosed_reports/main.py
@@ -0,0 +1,56 @@
+from argparse import ArgumentParser, FileType
+from typing import Iterator
+
+
+def parse_input(input_file) -> Iterator[list[int]]:
+    # Reads the input and returns two lists (left column, right column) as sorted ints
+    def number_generator(input_file):
+        for line in input_file:
+            yield list(map(int, line.split()))
+
+    return tuple(number_generator(input_file))
+
+
+def is_safe(report) -> bool:
+    # Given a list of integers, return True if the list is monotonic (either increasing or decreasing)
+    # and all differences between adjacent elements are 1, 2, or 3
+
+    diffs = [b - a for a, b in zip(report, report[1:])]
+
+    if set(diffs) <= {1, 2, 3} or set(diffs) <= {-1, -2, -3}:
+        return True
+
+    return False
+
+
+def is_safe_with_dampener(report) -> bool:
+    # Given a list of integers, return True if the list is monotonic (either increasing or decreasing)
+    # and all differences between adjacent elements are 1, 2, or 3, except for one element which can be removed
+    if is_safe(report):
+        return True
+
+    for i in range(len(report)):
+        if is_safe(report[:i] + report[i + 1 :]):
+            return True
+
+    return False
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--input", type=FileType("r"), required=True)
+    args = parser.parse_args()
+
+    reports = parse_input(args.input)
+
+    print("Part 1")
+    print("------")
+    print("Total safe: ", sum(is_safe(report) for report in reports))
+
+    print("\nPart 2")
+    print("------")
+    print(
+        "Total safe with dampener: ",
+        sum(
+            is_safe_with_dampener(report) for report in reports
+        ),
+    )
---
+++ 2_red_nosed_reports/test.py
@@ -0,0 +1,62 @@
+import unittest
+from main import is_safe, is_safe_with_dampener, parse_input
+
+
+class TestFunctions(unittest.TestCase):
+    PARSED_INPUT = (
+        [7, 6, 4, 2, 1],
+        [1, 2, 7, 8, 9],
+        [9, 7, 6, 2, 1],
+        [1, 3, 2, 4, 5],
+        [8, 6, 4, 4, 1],
+        [1, 3, 6, 7, 9],
+    )
+    INPUT_FILE = "test_input.txt"
+
+    def test_parse_input(self):
+        with open(self.INPUT_FILE, "r") as test_input:
+            result = parse_input(test_input)
+
+        self.assertEqual(result, self.PARSED_INPUT)
+
+    def test_is_safe(self):
+        expected_results = [True, False, False, False, False, True]
+
+        for i, report in enumerate(self.PARSED_INPUT):
+            result = is_safe(report)
+            self.assertEqual(result, expected_results[i])
+
+    def test_is_safe_with_dampener(self):
+        expected_results = [True, False, False, True, True, True]
+
+        for i, report in enumerate(self.PARSED_INPUT):
+            result = is_safe_with_dampener(report)
+            self.assertEqual(result, expected_results[i])
+
+    def test_is_safe_with_dampener_final_record(self):
+        # Tests that if just the final record is unsafe, the list is still considered safe
+        report = [1, 3, 6, 7, 7]
+        result = is_safe_with_dampener(report)
+        self.assertTrue(result)
+
+    def test_is_safe_with_dampener_final_two_records(self):
+        # Tests that if just the final record is unsafe, the list is still considered safe
+        report = [1, 3, 6, 7, 7, 7]
+        result = is_safe_with_dampener(report)
+        self.assertFalse(result)
+
+    def test_only_last_level_bad(self):
+        report = [63, 66, 67, 68, 69, 1]
+        result = is_safe_with_dampener(report)
+
+        self.assertTrue(result)
+
+    def test_only_first_level_bad(self):
+        report = [99, 66, 67, 68, 69, 70]
+        result = is_safe_with_dampener(report)
+
+        self.assertTrue(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
